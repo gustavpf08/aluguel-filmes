644478ce553f923062a7cf9e0954e935b12a98bc
Gustavo Paulino <gugapf08@gmail.com>
2025-03-20 10:03:03 -0300

    Alterando o nome dos botões

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-let buttonStatus1 = document.querySelector("#game-1");
-let valueButton1 = buttonStatus1.querySelector(".dashboard__item__button");
+let sectionMovie1 = document.querySelector("#game-1");
+let buttonStatus1 = sectionMovie1.querySelector(".dashboard__item__button");
 
-let buttonStatus2 = document.querySelector("#game-2");
-let valueButton2 = buttonStatus2.querySelector(".dashboard__item__button");
+let sectionMovie2 = document.querySelector("#game-2");
+let buttonStatus2 = sectionMovie2.querySelector(".dashboard__item__button");
 
-let buttonStatus3 = document.querySelector("#game-3");
-let valueButton3 = buttonStatus3.querySelector(".dashboard__item__button");
+let sectionMovie3 = document.querySelector("#game-3");
+let buttonStatus3 = sectionMovie3.querySelector(".dashboard__item__button");
 
 function changingTextArea(value) {
   if (value.innerHTML === "Alugar") {
@@ -15,22 +15,19 @@ function changingTextArea(value) {
   }
 }
 
-// deve ter uma forma de selecionar o #game-3 e, na mesma linha de código e
-// selecionar a ancora "a".
-
 function alterarStatus(value) {
   if (value === 1) {
-    valueButton1.classList.toggle("dashboard__item__button--return");
-    changingTextArea(valueButton1);
+    buttonStatus1.classList.toggle("dashboard__item__button--return");
+    changingTextArea(buttonStatus1);
   }
 
   if (value === 2) {
-    valueButton2.classList.toggle("dashboard__item__button--return");
-    changingTextArea(valueButton2);
+    buttonStatus2.classList.toggle("dashboard__item__button--return");
+    changingTextArea(buttonStatus2);
   }
 
   if (value === 3) {
-    valueButton3.classList.toggle("dashboard__item__button--return");
-    changingTextArea(valueButton3);
+    buttonStatus3.classList.toggle("dashboard__item__button--return");
+    changingTextArea(buttonStatus3);
   }
 }
