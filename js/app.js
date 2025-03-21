5c6cd521efee871b358aace0d58408d881e184c5
Gustavo Paulino <gugapf08@gmail.com>
2025-03-21 09:43:30 -0300

    Refatorando o código - mais dinâmico e conciso

--- js/app.js
+++ js/app.js
@@ -1,15 +1,3 @@
-let sectionMovie1 = document.querySelector("#game-1");
-let imgStatus1 = sectionMovie1.querySelector("div");
-let buttonStatus1 = sectionMovie1.querySelector(".dashboard__item__button");
-
-let sectionMovie2 = document.querySelector("#game-2");
-let imgStatus2 = sectionMovie2.querySelector("div");
-let buttonStatus2 = sectionMovie2.querySelector(".dashboard__item__button");
-
-let sectionMovie3 = document.querySelector("#game-3");
-let imgStatus3 = sectionMovie3.querySelector("div");
-let buttonStatus3 = sectionMovie3.querySelector(".dashboard__item__button");
-
 function changingTextArea(value) {
   if (value.innerHTML === "Alugar") {
     value.innerHTML = "Devolver";
@@ -19,21 +7,13 @@ function changingTextArea(value) {
 }
 
 function alterarStatus(value) {
-  if (value === 1) {
-    buttonStatus1.classList.toggle("dashboard__item__button--return");
-    imgStatus1.classList.toggle("dashboard__item__img--rented");
-    changingTextArea(buttonStatus1);
-  }
+  let sectionMovie = document.querySelector(`#game-${value}`);
+  let imgStatus = sectionMovie.querySelector("div");
+  let buttonStatus = sectionMovie.querySelector(".dashboard__item__button");
 
-  if (value === 2) {
-    buttonStatus2.classList.toggle("dashboard__item__button--return");
-    imgStatus2.classList.toggle("dashboard__item__img--rented");
-    changingTextArea(buttonStatus2);
-  }
-
-  if (value === 3) {
-    buttonStatus3.classList.toggle("dashboard__item__button--return");
-    imgStatus3.classList.toggle("dashboard__item__img--rented");
-    changingTextArea(buttonStatus3);
+  if (value == 1 || value == 2 || value == 3) {
+    buttonStatus.classList.toggle("dashboard__item__button--return");
+    imgStatus.classList.toggle("dashboard__item__img--rented");
+    changingTextArea(buttonStatus);
   }
 }
